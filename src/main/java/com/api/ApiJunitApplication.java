22bd83169436a79d209ffb97b2255ce04955dc68
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-01-27 08:44:33 -0300

    Projeto iniciado

---
+++ src/main/java/com/api/ApiJunitApplication.java
@@ -0,0 +1,13 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunitApplication.class, args);
+	}
+
+}
