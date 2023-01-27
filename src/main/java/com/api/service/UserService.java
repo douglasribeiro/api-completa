1e44ebccf61520668d0ddf458795063c34b1ab9c
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-01-27 11:01:53 -0300

    endpoint findAll Users

--- src/main/java/com/api/service/UserService.java
+++ src/main/java/com/api/service/UserService.java
@@ -2,7 +2,10 @@ package com.api.service;
 
 import com.api.domain.Users;
 
+import java.util.List;
+
 public interface UserService {
 
     Users findById(Integer id);
+    List<Users> findAll();
 }
--- src/main/java/com/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/api/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,10 @@ public class UserServiceImpl implements UserService {
     public Users findById(Integer id) {
         Optional<Users> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuario Não Encontrado."));
+    }
+
+    @Override
+    public List<Users> findAll() {
+        return userRepository.findAll();
     }
 }
